cedc509c51c581d96c8ce6e659f0772aa9155d4a
Nikunj Das Kasat <nikunjdaskasat@gmail.com>
2021-07-07 02:19:50 +0530

    Initial Commit

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,30 @@
+import "./post.css";
+import { Link } from "react-router-dom";
+
+export default function Post({ post }) {
+  const PF = "http://localhost:5000/images/";
+  return (
+    <div className="post">
+      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      <div className="postInfo">
+        <Link to={`/post/${post._id}`} className="link">
+          <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDesc">{post.desc}</p>
+      <div className="postCats">
+        {post.categories.map((c, i) => (
+          <span key={i} className="postCat">
+            <Link to={`/?cat=${c}`} className="link">
+              {c}
+            </Link>
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <li key={c._id} className="sidebarListItem">
+              <Link to={`/?cat=${c.name}`} className="link">
+                {c.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
